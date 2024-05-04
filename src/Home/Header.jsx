1dce62beb091c6055c7abf201866bcdfda0624de
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-05-04 03:49:06 +0100

    implement basic header (no functionality , basic styles)

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -1,5 +1,6 @@
 
-import { MenuOutlined } from "@ant-design/icons"
+import { MenuOutlined,SearchOutlined, FacebookOutlined, TwitterOutlined, InstagramFilled } from "@ant-design/icons"
+import { useState } from "react"
 
 
 
@@ -8,15 +9,31 @@ const listOfLinks = [{id:1,name:"MOVIES",link:""},
                      {id:3,name: "AT HOME",link:""},
                      {id:4,name: "CORPORATE",link:""}
                     ]
+const listOfSocials = [
+                        {id:0, icon:FacebookOutlined,link:""},
+                        {id:1, icon:TwitterOutlined,link:""},
+                        {id:2, icon:InstagramFilled,link:""},
+]
 
 function Header(){
-    return <div className="flex flex-col landscape:flex-row w-full  text-white">
-                <div className="flex w-full landscape:w-[50%] justify-between">
+    return <div className="flex flex-col landscape:flex-row w-full text-white justify-between">
+                <div className="flex w-full landscape:w-[60%] justify-between">
                     <Logo/>
-                    <NavList/>
+                    <div className="hidden landscape:block">
+                        <NavList/>
+                    </div>
                     <MenuTrigger/>
                 </div>
 
+                <div className="block landscape:flex justify-start gap-6 w-full pt-4 landscape:pt-0 landscape:w-[25%]">
+                    <SearchTrigger/>
+                    <div className="hidden landscape:block">
+                        <ThemeControls/>
+                    </div>
+                    <div className="hidden landscape:block">
+                        <Socials/>
+                    </div>
+                </div>
             </div>
 }
 
@@ -25,7 +42,7 @@ function Logo(){
 }
 
 function NavList({list = listOfLinks}){
-    return <div className="hidden landscape:flex ">
+    return <div className="flex flex-col landscape:gap-8  landscape:flex-row ">
                 {
                     list.map(function(aLinksObj){
                         return <p key={aLinksObj.id} >{aLinksObj.name}</p>
@@ -37,6 +54,29 @@ function NavList({list = listOfLinks}){
 function MenuTrigger(){
     return <div className="landscape:hidden">
                 <MenuOutlined/>
+            </div>
+}
+
+function SearchTrigger(){
+    return <div>
+                <SearchOutlined/>
+            </div>
+}
+function ThemeControls(){
+    const [toNightMode,settoNightMode] = useState(true)
+    return <div className="flex gap-1" >
+                <span><p className="text-slate-500">NIGHT MODE </p></span>
+                <span>  <p>{toNightMode?" ON":" OFF"}</p>    </span>
+            </div>
+}
+
+function Socials({socials=listOfSocials}){
+    return <div className="flex ml-[2rem] gap-4">
+                {
+                    socials.map(function(ASocialObj){
+                        return <span><ASocialObj.icon/></span>
+                    })
+                }
             </div>
 }
 
