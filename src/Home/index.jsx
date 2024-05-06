4fd19d07bbab355404999f650e3112c990785c7d
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-05-06 13:42:54 +0100

    implement landscape footer

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -2,13 +2,14 @@ import Header from "./Header";
 import MajorTrailer from "./MajorTrailer";
 import Categories from "./Categories";
 import Movies from "./Movies";
-
+import Footer from "./Footer";
 function Home(){
     return <div className=" min-h-screen bg-black relative w-screen box-border">
                 <MajorTrailer/>
                 <Header/>
                 <Categories/>
                 <Movies/>
+                <Footer/>
 
             </div>
 }
