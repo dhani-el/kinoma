b05d7ce5dfb32922dbb7bcd3c9c40bbecf132bd4
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-05-12 21:43:28 +0100

    make footer rsponsive

--- src/Home/Footer.jsx
+++ src/Home/Footer.jsx
@@ -8,7 +8,7 @@ const listOfSocials = [
 ]
 
 function Footer(){
-    return <div className="w-full flex justify-between border-[1rem] p-12 border-y-0 border-blue-500 bg-white hover:cursor-default mt-12 ">
+    return <div className="w-full flex landscape:gap-0 gap-6 landscape:flex-row flex-col justify-between border-[1rem] p-12 border-y-0 border-blue-500 bg-white hover:cursor-default mt-12 ">
                 <FooterLogo/>
                 <FooterList Head={"Coporate"} List={["News","About","Contact","Careers",]}/>
                 <FooterList Head={"Sitemap"} List={["News","About","Contact","Careers",]}/>
@@ -20,12 +20,12 @@ function Footer(){
 
 function FooterLogo(){
     return <div>
-                <p className="font-barbaropt text-3xl">KINOMA</p>
+                <p className="font-barbaropt text-5xl landscape:text-3xl">KINOMA</p>
             </div>
 }
 
 function FooterList({Head,List=[]}){
-    return <div className="flex flex-col gap-2 items-center ">
+    return <div className="flex flex-col gap-2 landscape:items-center ">
                 <p className="font-montserrat font-bold text-sm">{Head}</p>
                 {
                     List.map(function(single){
@@ -38,8 +38,8 @@ function FooterList({Head,List=[]}){
 function Subscribe(){
     return <div className="flex flex-col gap-4">
                 <p  className="font-montserrat font-bold text-sm">Subscribe</p>
-                <p className="font-montserrat text-[0.6rem] w-[70%] ">Subcribe to Our Newsleter to get Updates WhenMovies Release.</p>
-                <div className="border-b-[0.2rem] border-black w-[70%] flex">
+                <p className="font-montserrat text-[0.6rem] landscape:w-[70%] ">Subcribe to Our Newsleter to get Updates WhenMovies Release.</p>
+                <div className="border-b-[0.2rem] border-black landscape:w-[70%] flex">
                     <input type="email" placeholder="Enter Your Email" className="w-full border-transparent   font-montserrat text-sm outline-none" />
                     <ArrowRightOutlined className="text-slate-600" />
                 </div>
@@ -47,7 +47,7 @@ function Subscribe(){
 }
 
 function Socials({socials=listOfSocials}){
-    return <div className="flex ml-[2rem] gap-4">
+    return <div className="flex landscape:ml-[2rem] landscap:gap-4 w-full landscape:w-fit justify-between">
                 {
                     socials.map(function(ASocialObj){
                         return <span><ASocialObj.icon/></span>
