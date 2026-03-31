23ef51b1edad9a0a7d545bc88a0702f7a4b4e9af
daniel ukuhor <dani3lsavag3@gmail.com>
2026-03-31 18:33:51 +0100

    implement basic infinite loading

--- src/Home/components/infiniteLoadingTrigger.jsx
+++ src/Home/components/infiniteLoadingTrigger.jsx
@@ -1,14 +1,12 @@
-
+import { useState, useRef,useEffect } from "react";
 
 
 export default function InfiniteLoadingTrigger({callback}){
-    const [isLoading,setIsLoading] = useState(false);
     const triggerRef = useRef(null);   
     
     useEffect(function(){
         const observer = new IntersectionObserver(function(entries){
-            if (entries[0].isIntersecting && !isLoading){ 
-                setIsLoading(init=>true);
+            if (entries[0].isIntersecting){ 
                 callback();
             }   
         },{threshold:0.5})
@@ -20,7 +18,7 @@ export default function InfiniteLoadingTrigger({callback}){
                 observer.unobserve(triggerRef.current);
             }   
         }
-    },[callback,isLoading])
+    },[callback])
 
     return <div ref={triggerRef} className="w-full h-12 flex items-center justify-center">
                 <p className="text-sm text-slate-500 font-monetizer">Loading...</p>
